8c7033dc80859498ddf96f641f4099c48a87ab95
Doru Musuroi <doru.musuroi@gmail.com>
2016-10-26 23:28:46 +0300

    Increased automation of the exportToPdfImages script

--- exportToPdfImages.m
+++ exportToPdfImages.m
@@ -1,14 +1,14 @@
-director = './JPGs';
+director = {'./JPGs', '../data/imaginiTest', '../data/imaginiTestAlegere'};
 %director = '../data/imaginiTest';
 %director = '../data/imaginiTestAlegere';
 
-%mozaicuri = [dir([director '/*.' 'jpeg']); dir([director '/*.' 'JPG']); dir([director '/*.' 'png'])];
-mozaicuri = dir([director '/*.' 'png']);
-
-for idx = 1:length(mozaicuri)
-    imgNm = [director '/' mozaicuri(idx).name];
-    img = imread(imgNm);
-    mozaic = figure;
-    imshow(img);
-    export_fig(['PDFs/' mozaicuri(idx).name], mozaic, '-pdf');
+for idxDir = 1:length(director)
+    mozaicuri = [dir([director{idxDir} '/*.' 'jpeg']); dir([director{idxDir} '/*.' 'JPG']); dir([director{idxDir} '/*.' 'png'])];
+    for idx = 1:length(mozaicuri)
+        imgNm = [director{idxDir} '/' mozaicuri(idx).name];
+        img = imread(imgNm);
+        mozaic = figure;
+        imshow(img);
+        export_fig(['PDFs/' mozaicuri(idx).name], mozaic, '-pdf');
+    end
 end
