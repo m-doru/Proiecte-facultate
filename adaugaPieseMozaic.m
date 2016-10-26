42516d7fb195bbba0d708e8c5d9774fb63398bbc
Doru Musuroi <doru.musuroi@gmail.com>
2016-10-26 23:25:58 +0300

    Improved euclidian pixels distance selection case

--- adaugaPieseMozaic.m
+++ adaugaPieseMozaic.m
@@ -3,19 +3,16 @@ function imgMozaic = adaugaPieseMozaic(params)
 %tratati si cazul in care imaginea de referinta este gri (are numai un canal)
 
 [H,W,C,N] = size(params.pieseMozaic);
-[h,w,c] = size(params.imgReferintaRedimensionata)
+[h,w,c] = size(params.imgReferintaRedimensionata);
+
+imgMozaic = uint8(zeros(h,w,c));
 
 if c == 1
-    imgMozaic = uint8(zeros(h, w, 3));
-else
-    imgMozaic = uint8(zeros(h,w,c));
+  for i = 1:N
+    params.pieseMozaic(:,:,1,i) = rgb2gray(params.pieseMozaic(:,:,:,i));
+  end
+   params.pieseMozaic(:,:,2:end,:) = [];
 end
-%if c == 1
- %  for i = 1:N
-  %      params.pieseMozaic(:,:,1,i) = rgb2gray(params.pieseMozaic(:,:,:,i));
-   %end
-   %params.pieseMozaic(:,:,2:end,:) = [];
-%end
 
 switch(params.criteriu)
     case 'aleator'
@@ -49,7 +46,7 @@ switch(params.criteriu)
                 
                 distante = zeros(1, N);
                 for k = 1:N
-                    distante(k) = sqrt(sum((mediiPiese(:,:,:,k) - mediePixeliReferinta).^2));
+                    distante(k) = sqrt(sum(double((mediiPiese(:,:,:,k)) - double(mediePixeliReferinta)).^2));
                 end
                 [~, indexMinim] = min(distante);
                 
@@ -66,23 +63,36 @@ switch(params.criteriu)
         nrTotalPiese = params.numarPieseMozaicOrizontala * params.numarPieseMozaicVerticala;
         nrPieseAdaugate = 0;
         
+        normaPiese = zeros(N, 1);
+        for k = 1:N
+            piesa = params.pieseMozaic(:,:,:,k);
+            normaPiese(k) = sum((double(piesa(:))).^2);
+        end
+        
+        params.pieseMozaic = double(params.pieseMozaic);
+        
         for i = 1:params.numarPieseMozaicVerticala
             for j = 1:params.numarPieseMozaicOrizontala
                 pixeliReferinta = params.imgReferintaRedimensionata((i-1)*H+1:i*H,(j-1)*W+1:j*W,:);
                 
-                distante = zeros(1, N);
-                for k = 1:N
-                    if c == 1
-                        piesa = rgb2gray(params.pieseMozaic(:,:,:,k));
-                    else
-                        piesa = params.pieseMozaic(:,:,:,k);
-                    end
-                    distante(k) = sqrt(sum((piesa(:) - pixeliReferinta(:)).^2));
+                normaPixeliReferinta = sum((double(pixeliReferinta(:))).^2);
+                
+                dims = ones(1, length(size(pixeliReferinta)));
+                pixeliReferintaRepl = repmat(pixeliReferinta, [dims N]);
+                
+                sumVector = params.pieseMozaic .* double(pixeliReferintaRepl);
+                if length(size(sumVector)) == 4
+                    sumVector = sum(sumVector);
                 end
+                
+                dotProduct = sum(sum(sumVector));
+                
+                distante = reshape(normaPiese, [N 1])  + repmat(normaPixeliReferinta, [N 1]) - reshape(2*dotProduct, [N 1]);
+                
                 [~, indexMinim] = min(distante);
                 
                 imgMozaic((i-1)*H+1:i*H,(j-1)*W+1:j*W,:) = ...
-                    params.pieseMozaic(:,:,:, indexMinim);
+                    uint8(params.pieseMozaic(:,:,:, indexMinim));
                     
                 nrPieseAdaugate = nrPieseAdaugate+1;
                 fprintf('Construim mozaic ... %2.2f%% \n',100*nrPieseAdaugate/nrTotalPiese);
