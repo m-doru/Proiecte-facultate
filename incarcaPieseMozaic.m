f33b6176a18dc858d55755512abc2036328fe5dd
Doru Musuroi <doru.musuroi@gmail.com>
2016-10-28 00:35:09 +0300

    Modified incarcaPieseMozaic to allow initialization of pieces from array

--- incarcaPieseMozaic.m
+++ incarcaPieseMozaic.m
@@ -4,36 +4,40 @@ function pieseMozaic = incarcaPieseMozaic(params)
 %H = inaltime, W = latime, C = nr canale (C=1  gri, C=3 color)
 %functia intoarce pieseMozaic = matrice H x W x C x N
 %pieseMoziac(:,:,:,i) reprezinta piese numarul i 
-
 fprintf('Incarcam piesele pentru mozaic din director \n');
 %completati codul Matlab
+if ~isempty(params.numeDirector)
+    filelist = dir([params.numeDirector '/*.' params.tipImagine]);
 
-filelist = dir([params.numeDirector '/*.' params.tipImagine]);
+    imgNm = [params.numeDirector '/' filelist(1).name];
+    img = imread(imgNm);
 
-imgNm = [params.numeDirector '/' filelist(1).name];
-img = imread(imgNm);
+    pieseMozaic = uint8(zeros([size(img) length(filelist)]));
 
-pieseMozaic = uint8(zeros([size(img) length(filelist)]));
+    for idxImg = 1:length(filelist)
+        imgName = [params.numeDirector '/' filelist(idxImg).name];
+        nextImg = imread(imgName);
 
-for idxImg = 1:length(filelist)
-    imgName = [params.numeDirector '/' filelist(idxImg).name];
-    nextImg = imread(imgName);
-    
-    pieseMozaic(:,:,:,idxImg) = nextImg;
-end
-
-if params.afiseazaPieseMozaic
-    %afiseaza primele 100 de piese ale mozaicului
-    figure,
-    title('Primele 100 de piese ale mozaicului sunt:');
-    idxImg = 0;
-    for i = 1:10
-        for j = 1:10
-            idxImg = idxImg + 1;
-            subplot(10,10,idxImg);
-            imshow(pieseMozaic(:,:,:,idxImg));
-        end
+        pieseMozaic(:,:,:,idxImg) = nextImg;
     end
-    drawnow;
-    pause(2);
+
+else
+    pieseMozaic = params.pieseMozaic;
+end
+if params.afiseazaPieseMozaic
+        %afiseaza primele 100 de piese ale mozaicului
+        figure,
+        title('Primele 100 de piese ale mozaicului sunt:');
+        idxImg = 0;
+        for i = 1:10
+            for j = 1:10
+                idxImg = idxImg + 1;
+                subplot(10,10,idxImg);
+                imshow(pieseMozaic(:,:,:,idxImg));
+            end
+        end
+        drawnow;
+        pause(2);
+end
+    
 end
