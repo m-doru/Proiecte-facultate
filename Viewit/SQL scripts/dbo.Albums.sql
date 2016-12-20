3de4535a8f360a85dd0789782ee8117e420f55c8
Doru Musuroi <doru.musuroi@gmail.com>
2016-12-21 00:04:09 +0200

    Add sql scripts

---
+++ Viewit/SQL scripts/dbo.Albums.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Albums] (
+    [Id]      INT           IDENTITY (1, 1) NOT NULL,
+    [id_user] INT           NOT NULL REFERENCES Users(Id),
+    [name]    VARCHAR (255) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Viewit/SQL scripts/dbo.Images.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Images]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY (1,1), 
+    [uploader_id] INT NOT NULL REFERENCES Users(Id), 
+    [url] VARCHAR(255) NOT NULL, 
+    [upload_date] DATE NOT NULL, 
+    [description] VARCHAR(MAX) NULL, 
+    [city] INT NULL REFERENCES Cities(Id),
+
+	
+)
