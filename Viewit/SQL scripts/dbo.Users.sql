db9b70d3a8457b4423d3dc2be5ec7d04840bdbc8
Doru Musuroi <doru.musuroi@gmail.com>
2016-12-22 23:58:33 +0200

    Update Users table script

--- Viewit/SQL scripts/dbo.Users.sql
+++ Viewit/SQL scripts/dbo.Users.sql
@@ -1,11 +1,12 @@
 ﻿CREATE TABLE [dbo].[Users] (
     [Id]         INT           IDENTITY (1, 1) NOT NULL,
+    [username]  VARCHAR (50)  NOT NULL,
     [first_name] VARCHAR (50)  NOT NULL,
     [last_name]  VARCHAR (50)  NOT NULL,
     [birthday]   DATE          NOT NULL,
     [email]      VARCHAR (50)  NOT NULL,
     [password]   VARCHAR (255) NOT NULL,
-    [id_admin]   BIT           NOT NULL,
+    [is_admin]   BIT           NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
