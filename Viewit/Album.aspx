a3a32d8b891295cd18f95ad73c45934e6353bbdf
Doru Musuroi <doru.musuroi@gmail.com>
2016-12-30 11:03:24 +0200

    Finished Album and Categories Page

--- Viewit/Album.aspx
+++ Viewit/Album.aspx
@@ -1,16 +1,16 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Viewit.Master" AutoEventWireup="true" CodeBehind="Profile.aspx.cs" Inherits="Viewit.Profile" MaintainScrollPositionOnPostback="true" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Viewit.Master" AutoEventWireup="true" CodeBehind="Album.aspx.cs" Inherits="Viewit.Album" MaintainScrollPositionOnPostback="true"%>
 <asp:Content ID="Content1" ContentPlaceHolderID="Head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="Top" runat="server">
     <br />
-    <asp:Label id="UserGreeting" runat="server"></asp:Label>
+    <asp:Label id="UserGreeting" runat="server" ></asp:Label>
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="Left" runat="server" >
     <asp:BulletedList id="UserAlbumsList" runat="server"></asp:BulletedList>
 </asp:Content>
 <asp:Content ID="Content4" ContentPlaceHolderID="Main" runat="server">
-    <asp:ScriptManager ID="UserImagesScriptManger" runat="server" EnablePartialRendering="true"></asp:ScriptManager>
-    <asp:UpdatePanel ID="UserImagesPanel" runat="server">
+    <asp:ScriptManager ID="AlbumImagesScriptManger" runat="server" EnablePartialRendering="true"></asp:ScriptManager>
+    <asp:UpdatePanel ID="AlbumImagesPanel" runat="server">
         <ContentTemplate>
             <fieldset>
                 <asp:Panel id="ThumbnailsHolder" runat="server" HorizontalAlign="Center"></asp:Panel>
@@ -21,4 +21,6 @@
 </asp:Content>
 <asp:Content ID="Content5" ContentPlaceHolderID="Right" runat="server">
     <a href="Upload.aspx">Upload an image :)</a>
+    <br />
+    <a href="Profile.aspx">My profile</a>
 </asp:Content>
--- Viewit/Album.aspx.cs
+++ Viewit/Album.aspx.cs
@@ -4,38 +4,37 @@ using System.Web.UI.WebControls;
 
 namespace Viewit
 {
-    public partial class Profile : System.Web.UI.Page
+    public partial class Album : System.Web.UI.Page
     {
-        private string profileUser;
         private const int NR_OF_APPENDED_IMAGES = 3;
+        private App_Code.User profileUser;
+        private int albumId;
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
                 Session["LastAppendedImg"] = 0;
 
-                if (Request["username"] == null && Session["username"] == null)
+                if (Request["user"] == null || Request["album"] == null)
                 {
                     Response.Redirect("Login.aspx");
-                }
-                if (Request["username"] != null)
-                {
-                    profileUser = (string)Request["username"];
+                    return;
                 }
                 else
                 {
-                    profileUser = (string)Session["username"];
-                }
-                if (Session["username"] != null)
-                {
-                    string loggedUser = (string)Session["username"];
-                    if (loggedUser == profileUser)
+                    profileUser = App_Code.SqlUtilities.GetUser(int.Parse(Request["user"]));
+                    albumId = int.Parse(Request["album"]);
+                    if (Session["username"] != null)
                     {
-                        UserGreeting.Text = "Hello, " + loggedUser;
-                    }
-                    else
-                    {
-                        UserGreeting.Text = "Hello, " + loggedUser + ". Welcome to " + profileUser + "'s profile";
+                        string loggedUser = (string)Session["username"];
+                        if (loggedUser == profileUser.Username)
+                        {
+                            UserGreeting.Text = "My albums";
+                        }
+                        else
+                        {
+                            UserGreeting.Text = "Hello, " + loggedUser + ". Welcome to " + profileUser + "'s album";
+                        }
                     }
                 }
                 AddAlbumsToLeftPlaceholder();
@@ -43,23 +42,14 @@ namespace Viewit
             }
             else
             {
-                if (Request["username"] != null)
-                {
-                    profileUser = Request["username"];
-                }
-                else
-                {
-                    profileUser = (string)Session["username"];
-                }
+                albumId = int.Parse(Request["album"]);
+                profileUser = App_Code.SqlUtilities.GetUser(int.Parse(Request["user"]));
             }
-
         }
         protected void AppendThumbnailsToMainPlaceholder(object sender, EventArgs e)
         {
-
             int lastAppendedImage = (int)Session["LastAppendedImg"];
-            int profileUserId = App_Code.SqlUtilities.GetUserId(profileUser);
-            List<App_Code.Image> images = App_Code.SqlUtilities.GetImagesOrderedByDate(profileUserId, 1, lastAppendedImage + NR_OF_APPENDED_IMAGES);
+            List<App_Code.Image> images = App_Code.SqlUtilities.GetAlbumImagesOrderedByDate(albumId, 1, lastAppendedImage + NR_OF_APPENDED_IMAGES);
 
             foreach (App_Code.Image img in images)
             {
@@ -69,7 +59,6 @@ namespace Viewit
                 currImg.Height = 600;
                 currImg.Width = 500;
                 currImg.OnClientClick = "ThumbnailClick";
-                //currImg.CssClass = "thumbnails";
                 currImg.BorderWidth = 20;
                 currImg.BorderColor = System.Drawing.Color.White;
 
@@ -79,10 +68,9 @@ namespace Viewit
             lastAppendedImage += images.Count;
             Session["LastAppendedImg"] = lastAppendedImage;
         }
-
         private void AddAlbumsToLeftPlaceholder()
         {
-            List<App_Code.Album> albums = App_Code.SqlUtilities.GetAlbums(profileUser);
+            List<App_Code.Album> albums = App_Code.SqlUtilities.GetAlbums(profileUser.Username);
             UserAlbumsList.DisplayMode = BulletedListDisplayMode.HyperLink;
 
             foreach (App_Code.Album album in albums)
