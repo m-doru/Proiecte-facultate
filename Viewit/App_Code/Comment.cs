2d3b85fa5f49a41b25a79c574f5ec19d5f44b7cf
Doru Musuroi <doru.musuroi@gmail.com>
2017-01-04 12:17:49 +0200

    Create comment class

---
+++ Viewit/App_Code/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Viewit.App_Code
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ImageId { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+
+        public Comment(int id, int userId, int imgId, string content, DateTime date)
+        {
+            Id = id;
+            UserId = userId;
+            ImageId = imgId;
+            Content = content;
+            Date = date;
+        }
+    }
+}
