15a3ff076adb7e90fbfc7d45a51f4d5a0fa0d853
Doru Musuroi <doru.musuroi@gmail.com>
2017-01-05 17:49:41 +0200

    Add class for Database image search

---
+++ Viewit/App_Code/DbImagesSearchQuerry.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Viewit.App_Code
+{
+    public class DbImagesSearchQuerry
+    {
+        public StringBuilder Query { get; }
+        private SqlCommand SqlCmd;
+        public DbImagesSearchQuerry()
+        {
+            Query = new StringBuilder();
+            Query.Append("SELECT im.id FROM images im");
+            SqlCmd = new SqlCommand();
+
+        }
+        public void AddAlbum()
+        {
+            Query.Append(" JOIN included_in inc ON im.id = inc.id_image JOIN albums ab ON ab.id = inc.id_album");
+        }
+
+        public void AddCategory()
+        {
+            Query.Append(" JOIN belongs_to blg ON im.id = blg.id_image JOIN categories categ ON categ.id = blg.id_category");
+        }
+
+        public void AddCondition(string key, string value)
+        {
+            if (Query.ToString().Contains("WHERE"))
+            {
+                Query.Append(" AND " + key + "=" + "@" + key);
+            }
+            else
+            {
+                Query.Append(" WHERE " + key + "=" + "@" + key);
+            }
+            SqlCmd.Parameters.Add(new SqlParameter("@" + key, TypeCode.String));
+            SqlCmd.Parameters["@" + key].Value = value;
+        }
+        public SqlCommand GetSqlCommand()
+        {
+            if(SqlCmd.CommandText == null || string.IsNullOrEmpty(SqlCmd.CommandText))
+            {
+                SqlCmd.CommandText = Query.ToString();
+            }
+            return SqlCmd;
+        }
+    }
+}
